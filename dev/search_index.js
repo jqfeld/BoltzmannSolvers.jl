d3c69ee2db8f1b9fed5006e0a9bb10b69f0d516d
Documenter.jl <documenter@juliadocs.github.io>
2023-05-06 14:04:48 +0000

    build based on 3ef3546

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BoltzmannSolvers","category":"page"},{"location":"#BoltzmannSolvers","page":"Home","title":"BoltzmannSolvers","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BoltzmannSolvers.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BoltzmannSolvers]","category":"page"}]
+}
