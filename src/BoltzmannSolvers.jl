acbd24a6ab8775fe373eca44ed6bf550c7ebbe62
Jan Kuhfeld <jan.kuhfeld@rub.de>
2023-05-31 20:31:32 +0200

    refactor: remove unused exports

--- src/BoltzmannSolvers.jl
+++ src/BoltzmannSolvers.jl
@@ -9,15 +9,19 @@ load_raw_dataframe(::Solver, source) = error("Not implemented for this solver.")
 default_swarm_names(::Solver) = error("Not implemented for this solver.")
 
 
-function load_dataframe(s::S, source; kwargs...) where S <: Solver
+function load_dataframe(s::S, source; 
+    replacements=nothing, kwargs...) where S <: Solver
     df = load_raw_dataframe(s, source; kwargs...)
     rename!(df, default_swarm_names(s)...)
     reaction_names = parse_reaction_names(s, source)
     rename!(df, reaction_names...)
+    if !isnothing(replacements)
+        rename!(df, names(df) .=> replace.(names(df), replacements))
+    end 
     return df
 end 
 
-export load_dataframe, default_names
+export load_dataframe
 export create_interpolation
 
 end
