f33106bcbcfd6261c9195adbf326ab5d8ea370b8
Jan Kuhfeld <jan.kuhfeld@rub.de>
2023-06-01 15:48:40 +0200

    add normalization (nop without PlasmaSpecies)

--- src/BoltzmannSolvers.jl
+++ src/BoltzmannSolvers.jl
@@ -8,9 +8,12 @@ include("interpolation.jl")
 load_raw_dataframe(::Solver, source) = error("Not implemented for this solver.")
 default_swarm_names(::Solver) = error("Not implemented for this solver.")
 
+normalize(x) = x
 
 function load_dataframe(s::S, source; 
-    replacements=nothing, kwargs...) where S <: Solver
+    replacements=nothing, 
+    normalize=false,
+    kwargs...) where S <: Solver
     df = load_raw_dataframe(s, source; kwargs...)
     rename!(df, default_swarm_names(s)...)
     reaction_names = parse_reaction_names(s, source)
@@ -18,6 +21,11 @@ function load_dataframe(s::S, source;
     if !isnothing(replacements)
         rename!(df, names(df) .=> replace.(names(df), replacements))
     end 
+
+    if normalize
+        rename!(df, names(df) .=> normalize.(names(df)))
+    end
+
     return df
 end 
 
