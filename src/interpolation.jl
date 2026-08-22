b2daa7e664f9554df114c366c2cb9a39765940e3
Jan Kuhfeld <jankuhfeld@plasma-matters.nl>
2026-08-22 08:27:52 +0200

    update upstream packages

--- src/interpolation.jl
+++ src/interpolation.jl
@@ -4,7 +4,7 @@ struct NamedInterpolation{I}
     name::Symbol
     itp::I
 end
-NamedInterpolation(name, y, x) = NamedInterpolation(name,LinearInterpolation(y,x; extrapolate=true))
+NamedInterpolation(name, y, x) = NamedInterpolation(name,LinearInterpolation(y,x; extrapolation=ExtrapolationType.Constant))
 
 Base.nameof(i::NamedInterpolation) = i.name
 Base.show(io::IO, i::NamedInterpolation) = Base.print(io::IO, "[$(i.name)]")
